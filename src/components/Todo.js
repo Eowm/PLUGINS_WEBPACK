f68d9fbe09043106af38993ceafd0230180da4e0
Adam Nelip <Adam.Nelip@gmail.com>
2018-08-08 20:03:35 +0200

    Not working adiing tasks

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,4 +2,4 @@ import React from 'react';
 
 const Todo = props => <li key={props.id} id={props.id} onClick={() => props.remove(props.id)}>{props.text}</li>
 
-export Default Todo;
+export default Todo;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,7 +3,7 @@ import Todo from './Todo.js';
 import style from './TodoList.css';
 
 const TodoList = props => {
-	const todoListElments = props.list.map(element => <Todo id={element.id} key={element.id} remove={props.remove} text={element.text}>)
+	const todoListElments = props.list.map(element => <Todo id={element.id} key={element.id} remove={props.remove} text={element.text} />)
 		return (
 		<div className={style.TodoList}>
 			<ul>{todoListElments}</ul>
