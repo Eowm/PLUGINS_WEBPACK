1e6caab574549d445b75745f1b9f9a7942ff01f3
Adam Nelip <Adam.Nelip@gmail.com>
2018-08-08 21:17:51 +0200

    not wroking still

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -5,7 +5,7 @@ const TodoForm = (props) => (
 				<label>What do you need to do?</label>
 				<input type='text' id='task' value={props.value} onChange={(event) => props.updateValue(event.target.value)} />
 				<button onClick={event => {
-					props.add(props.value, props.event);
+					 return props.add(props.value, props.event);
 				}>Just add it!</button>
 			</form>
 			);
